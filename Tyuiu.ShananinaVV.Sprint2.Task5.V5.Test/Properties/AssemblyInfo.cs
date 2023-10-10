b4ef9ae50b06be8ae56cea90c02f66e3da4f7e0e
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-10-10 18:08:17 +0500

    Task5

---
+++ Tyuiu.ShananinaVV.Sprint2.Task5.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShananinaVV.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShananinaVV.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc19caeb-40cf-4011-9fa3-d05145462841")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShananinaVV.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShananinaVV.Sprint2.Task5.V5.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint2.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Шананина В. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Шананина Валерия Викторовна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch,                 *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("* Игральным картам условно присвоены следующие порядковые номера в        *");
+            Console.WriteLine("* зависимости от их достоинства: «валету» — 11, «даме» — 12,              *");
+            Console.WriteLine("* «королю» — 13, «тузу» — 14. Порядковые номера остальных карт            *");
+            Console.WriteLine("* соответствуют их названиям («шестерка», «девятка» и т. п.).             *");
+            Console.WriteLine("* По заданному номеру карты k (6 <=k <=14) определить достоинство         *");
+            Console.WriteLine("* соответствующей карты.                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+
+           
+
+            Console.WriteLine("Введите номер карты : ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numCard < 6 )||(numCard>14))
+            {
+                res = "Введено неверное значение";
+
+            }
+            else
+            {
+                res = "Достоинство карты : " + ds.FindCardValue(numCard);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}

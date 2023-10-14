4036da140a3fc39306c35bd3ffd61189f47bcc72
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-10-14 20:18:37 +0500

    Task7

--- Tyuiu.ShananinaVV.Sprint2.Task7.V11.Test/DataServiceTest.cs
+++ Tyuiu.ShananinaVV.Sprint2.Task7.V11.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.ShananinaVV.Sprint2.Task7.V11.Test
         {
             DataService ds = new DataService();
 
-            double x = 0.8;
+            double x = 0.5;
             double y = 0.8;
             bool res = ds.CheckDotInShadedArea(x, y);
             bool wait = true;
